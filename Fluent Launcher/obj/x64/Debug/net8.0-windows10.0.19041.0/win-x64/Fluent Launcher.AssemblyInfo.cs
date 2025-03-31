70a31bf3544196c5ff64e47dcbe5561858ae9dfb
LaoYu <yukougou@gmail.com>
2025-03-31 13:28:25 +0900

    修改了UI布局

--- Fluent Launcher/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Fluent Launcher.AssemblyInfo.cs
+++ Fluent Launcher/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Fluent Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fluent Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6848e87381326925c534206973d6bcb3092db17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de97791cd7ae2c2406ee2c4f2c28db8138dfe04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fluent Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fluent Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
